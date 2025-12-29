110092f512de7e02e3d500265bf27445cb0411f2
VHISTORE <vibemusic1712@gmail.com>
2025-12-29 21:53:21 +0200

    Create firebase-messaging-sw.js

---
+++ firebase-messaging-sw.js
@@ -0,0 +1,25 @@
+importScripts('https://www.gstatic.com/firebasejs/10.7.1/firebase-app-compat.js');
+importScripts('https://www.gstatic.com/firebasejs/10.7.1/firebase-messaging-compat.js');
+
+firebase.initializeApp({
+    apiKey: "AIzaSyCQxz47mev45XXLz3ejJViVQCzFL_Fo3z8",
+    authDomain: "ursaipa.firebaseapp.com",
+    projectId: "ursaipa",
+    storageBucket: "ursaipa.firebasestorage.app",
+    messagingSenderId: "697377996977",
+    appId: "1:697377996977:web:f94ca78dfe3d3472942290"
+});
+
+const messaging = firebase.messaging();
+
+// Фоновая обработка уведомления
+messaging.onBackgroundMessage((payload) => {
+    console.log('Received background message ', payload);
+    const notificationTitle = payload.notification.title;
+    const notificationOptions = {
+        body: payload.notification.body,
+        icon: payload.notification.image || '/icons/logoursa.jpeg'
+    };
+
+    self.registration.showNotification(notificationTitle, notificationOptions);
+});
